369d0bf9ae4221dfa736a993ae539860017b258a
moisesiriarte27 <moisesiriarte27@gmail.com>
2025-09-30 18:30:56 -0300

    last

--- src/main/java/com/example/my_API/config/SecurityConfig.java
+++ src/main/java/com/example/my_API/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(origenPermitido.split(",")));
+        configuration.setAllowedOrigins(List.of(origenPermitido));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-auth-token"));
         configuration.setAllowCredentials(true);
